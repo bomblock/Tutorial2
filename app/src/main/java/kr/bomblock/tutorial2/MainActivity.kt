110af4b80db9ea1a31e5503c8c343be8648b5648
bomblock <ekwotmd@gmail.com>
2023-02-19 18:34:52 +0900

    Initial commit

---
+++ app/src/main/java/kr/bomblock/tutorial2/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.bomblock.tutorial2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    public void addClick(View v){
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+    }
+}
